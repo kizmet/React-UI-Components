bf259fa749fd090b72bbf050d9361cc6c50e9a5f
Kizmet <bryantpatton@gmail.com>
2019-05-28 15:56:14 -0700

    project 1

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footer"> 
+        <ul id="footerlist">
+        <li id="f1"><a href="#"></a> </li>
+        <li id="f2"><a href="#"></a> </li>
+        <li id="f3"><a href="#"></a> </li>
+        <li id="f4"><a href="#"></a> </li>
+        
+        </ul>
+        </div>
+
+        );
+}
+
+export default Footer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+  <div className="main">
+    <div className="title">
+      <h5>Lambda School</h5>
+      <h6>@lambdaschool * 28 May</h6>
+
+
+    </div>
+    <p>Let's learn React by building simple interfaces with components. Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React!</p>
+
+  </div>
+        
+
+        )
+}
+
+export default HeaderTitle;
